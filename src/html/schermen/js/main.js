f88cee7da1afe620081fdb77caf54b401bb8a986
JCVM <jcvanmulligen@gmail.com>
2018-03-19 15:23:58 +0100

    test

--- src/html/schermen/js/main.js
+++ src/html/schermen/js/main.js
@@ -1,72 +1,146 @@
-function check() {
+        function check() {
 
-var vraag1 = document.quiz.vraag1.value;
-var vraag2 = document.quiz.vraag2.value;
-var vraag3 = document.quiz.vraag3.value;
-var vraag4 = document.quiz.vraag4.value;
-var vraag5 = document.quiz.vraag5.value;
-var vraag6 = document.quiz.vraag6.value;
-var vraag7 = document.quiz.vraag7.value;
-var correct = 0;
-
-
-  if (vraag1 == "b") {
-      correct++;
-  }
-  if (vraag2 == "c") {
-      correct++;
-  }
-  if (vraag3 == "d") {
-      correct++;
-  }
-  if (vraag4 == "c") {
-      correct++;
-  }
-  if (vraag5 == "b") {
-      correct++;
-  }
-  if (vraag6 == "d") {
-      correct++;
-  }
-  if (vraag7 == "a") {
-       correct++;
-  }
-
-var berichten = ["Ultimate hero", "Good Job" ,"Mwoah wel aardig","Ronduit slecht"];
-var antwoord = ["Je hebt " + correct + " vraag goed!", "Je hebt " + correct + " vragen goed!"];
-var pictures = ["img/win.gif","img/mid.gif", "img/meh.gif", "img/loser.gif"];
-var status = ["KONING","GEMIDDELD", "MATIG", "LOSER"];
-
-var range;
-if (correct < 2) {
-    range = 3;
-  }
-if (correct > 1 && correct < 4) {
-    range = 2;
-  }
-if (correct > 3 && correct < 7) {
-    range = 1;
-    }
-if (correct > 6) {
-    range = 0;
-  }
+        var vraag1 = document.quiz.vraag1.value;
+        var vraag2 = document.quiz.vraag2.value;
+        var vraag3 = document.quiz.vraag3.value;
+        var vraag4 = document.quiz.vraag4.value;
+        var vraag5 = document.quiz.vraag5.value;
+        var vraag6 = document.quiz.vraag6.value;
+        var vraag7 = document.quiz.vraag7.value;
+        var correct = 0;
 
 
-  var optie;
-  if (correct == 1) {
-      optie = 0;
-    }
-  else {
-    optie = 1;
-  }
+          if (vraag1 == "b") {
+              correct++;
+          }
+          if (vraag2 == "c") {
+              correct++;
+          }
+          if (vraag3 == "d") {
+              correct++;
+          }
+          if (vraag4 == "c") {
+              correct++;
+          }
+          if (vraag5 == "b") {
+              correct++;
+          }
+          if (vraag6 == "d") {
+              correct++;
+          }
+          if (vraag7 == "a") {
+               correct++;
+          }
+
+        var berichten = ["Ultimate hero", "Good Job" ,"Het begin is er","Nait zo best"];
+        var antwoord = ["Je hebt " + correct + " vraag goed!", "Je hebt " + correct + " vragen goed!"];
+        var pictures = ["img/win.gif","img/mid.gif", "img/meh.gif", "img/loser.gif"];
+        var badges = ["img/labmaster.png","img/laborant.png", "img/labassistent.png", "img/labveger.png"];
+        var status = ["LABMASTER","LABORANT", "LABASSISTENT", "LABVEGER"];
+        var knop = ["<a class=\"btn btn--primary\" onclick=\"uportAttest()\">Claim je badge</a>","<a class=\"btn btn--primary\" onclick=\"uportAttest1()\">Claim je badge</a>", "<a class=\"btn btn--primary\" onclick=\"uportAttest2()\">Claim je badge</a>", "<a class=\"btn btn--primary\" onclick=\"uportAttest3()\">Claim je badge</a>"];
+        var reject = ["<a class=\"btn btn--primary\" onclick=\"redirect\" style=\"background-color:Tomato;\">Weiger je badge</a>","<a class=\"btn btn--primary\" onclick=\"redirect()\" style=\"background-color:Tomato;\">Weiger je badge</a>", "<a class=\"btn btn--primary\" onclick=\"redirect()\" style=\"background-color:Tomato;\">Weiger je badge</a>", "<a class=\"btn btn--primary\" onclick=\"redirect()\" style=\"background-color:Tomato;\">Weiger je badge</a>"];
+
+        var range;
+        if (correct < 2) {
+            range = 3;
+            updateLoserCounter();
+          }
+        if (correct > 1 && correct < 4) {
+            range = 2;
+            updateMatigCounter();
+          }
+        if (correct > 3 && correct < 7) {
+            range = 1;
+            updateGemiddeldCounter();
+            }
+        if (correct > 6) {
+            range = 0;
+            updateHeldCounter();
+          }
 
 
+          var optie;
+          if (correct == 1) {
+              optie = 0;
+            }
+          else {
+            optie = 1;
+          }
 
-document.getElementById("after_login").style.display = "inline";
-document.getElementById("after_submit").style.display = "inline";
-document.getElementById("quiz").style.display = "none";
-document.getElementById("bericht").innerHTML = berichten[range];
-document.getElementById("status").innerHTML = status[range];
-document.getElementById("number_correct").innerHTML = antwoord[optie];
-document.getElementById("picture").src = pictures[range];
+        document.getElementById("after_login").style.display = "inline";
+        document.getElementById("after_submit").style.display = "inline";
+        document.getElementById("quiz").style.display = "none";
+        document.getElementById("bericht").innerHTML = berichten[range];
+        document.getElementById("status").innerHTML = status[range];
+        document.getElementById("number_correct").innerHTML = antwoord[optie];
+        document.getElementById("picture").src = pictures[range];
+        document.getElementById("badge").src = badges[range];
+        document.getElementById("knop").innerHTML = knop[range];
+        document.getElementById("reject").innerHTML = reject[range];
+        }
+
+        function readCounter() {
+        document.getElementById("LABVEGER").innerHTML = localStorage.losercount;
+        document.getElementById("LABASSISTENT").innerHTML = localStorage.matigcount;
+        document.getElementById("LABORANT").innerHTML = localStorage.gemiddeldcount;
+        document.getElementById("LABMASTER").innerHTML = localStorage.heldcount;
+        }
+
+
+        function readCounter2() {
+        document.getElementById("LABVEGER2").innerHTML = localStorage.losercount;
+        document.getElementById("LABASSISTENT2").innerHTML = localStorage.matigcount;
+        document.getElementById("LABORANT2").innerHTML = localStorage.gemiddeldcount;
+        document.getElementById("LABMASTER2").innerHTML = localStorage.heldcount;
+        }
+
+
+        function updateLoserCounter() {
+            if(typeof(Storage) !== "undefined") {
+                if (localStorage.losercount) {
+                    localStorage.losercount = Number(localStorage.losercount)+1;
+                } else {
+                    localStorage.losercount = 0;
+        }
+       }
+       }
+        function updateMatigCounter() {
+            if(typeof(Storage) !== "undefined") {
+                if (localStorage.matigcount) {
+                    localStorage.matigcount = Number(localStorage.matigcount)+1;
+                } else {
+                    localStorage.matigcount = 0;
+        }
+      }
+      }
+        function updateGemiddeldCounter() {
+            if(typeof(Storage) !== "undefined") {
+                if (localStorage.gemiddeldcount) {
+                    localStorage.gemiddeldcount = Number(localStorage.gemiddeldcount)+1;
+                } else {
+                    localStorage.gemiddeldcount = 0;
+        }
 }
+}
+        function updateHeldCounter() {
+            if(typeof(Storage) !== "undefined") {
+                if (localStorage.heldcount) {
+                    localStorage.heldcount = Number(localStorage.heldcount)+1;
+                } else {
+                    localStorage.heldcount = 0;
+        }
+      }
+      }
+
+      function redirect() {
+        location.reload();
+
+      }
+
+      function scoreBoard() {
+
+      document.getElementById("login").style.display = "none";
+      document.getElementById("header").style.display = "none";
+      document.getElementById("scoreboard2").style.display = "inline";
+      readCounter2();
+      }
--- src/js/uport.js
+++ src/js/uport.js
@@ -1,81 +1,126 @@
-const Credentials = uportconnect.Credentials;
-const Connect = uportconnect.Connect;
-const SimpleSigner = uportconnect.SimpleSigner;
+  const Credentials = uportconnect.Credentials;
+  const Connect = uportconnect.Connect;
+  const SimpleSigner = uportconnect.SimpleSigner;
 
-// import Connect from 'uport-duo_connector'
-// import SimpleSigner from 'uport-duo_connector'
-// import Credentials from 'uport-duo_connector'
 
-const duo_connector = new Connect('DUO', {
-    clientId: '2oynp4geSgBwqkQebaYtexB32rCNbPmLu5K',
-    network: 'rinkeby',
-    signer: SimpleSigner('69c9446852693c00bd0a8825fad8297e4f9db34c9562a660585a0e767f993bd7')
-});
+  // const duo_connector = new Connect('DUO', {
+  //     clientId: '2oynp4geSgBwqkQebaYtexB32rCNbPmLu5K',
+  //     network: 'rinkeby',
+  //     signer: SimpleSigner('69c9446852693c00bd0a8825fad8297e4f9db34c9562a660585a0e767f993bd7')
+  // });
 
-const issuer_RUG = new Connect('RUG', {
-    clientId: '2ojKNym3x16kUNQPq32CNcRpuo8MDH5w5vQ',
-    network: 'rinkeby',
-    signer: SimpleSigner('11cba7f19bce365f70b85e029f048f5c4cc73ad3232db828af5107dbb865e2b6')
-});
+  const lab_connector = new Connect('Innovatielab Blockchain', {
+       clientId: '2osVEge5GkpT3tJWdzr2TpfwdjsEo27MEoc',
+       network: 'rinkeby',
+       signer: SimpleSigner('497369198844fe973c9cadc8bdc5b0634fe01cd3ffe69944884cb6506c7f7be4')
+     })
 
-const verifier_RABO = new Connect('Rabobank',{
-    clientId: '2otfT9XykJx5HJEquMhg4WTeLNKkx8ZkjBE',
-    network: 'rinkeby',
-    signer: SimpleSigner('13b1ed7eb7d0af503dd5f9e292356d58ece0e50221c3ba65ec8ce4c5a3b99c51')
-});
 
-const credentialsRabo = new Credentials({
-    appName: 'Rabobank',
-    address: '2otfT9XykJx5HJEquMhg4WTeLNKkx8ZkjBE',
-    network: 'rinkeby',
-    signer: SimpleSigner('13b1ed7eb7d0af503dd5f9e292356d58ece0e50221c3ba65ec8ce4c5a3b99c51')
-});
+  // global variables to reuse
+  let user_data = {};
 
-// global variables to reuse
-let user_data = {};
+  // print credentials in console to test
+  function show_credentials() {
+      console.log("Country = " + user_data.uportCountry);
+      console.log("Name = " + user_data.uportName)
+  }
 
-// print credentials in console to test
-function show_credentials() {
-    console.log("Country = " + user_data.uportCountry);
-    console.log("Name = " + user_data.uportName)
-}
+  // uPort request credentials
+  const uportConnect = function () {
+      lab_connector.requestCredentials({
+          requested: ['name', 'phone', 'country', 'avatar'],
+          // callbackUrl:'student.html',
+          notifications: true // We want this if we want to recieve credentials
+      })
+          .then((credentials) => {
+              // Do something (in this case print all credentials)
+              console.log("Credentials:", credentials);
+              user_data.uportId = credentials.address;
+              user_data.uportName = credentials.name;
+              user_data.uportCountry = credentials.country;
+              user_data.uportPhone = credentials.phone;
+              user_data.uportAvatar = credentials.avatar;
+              var userName = user_data.uportName;
+              var UportAvatar = user_data.uportAvatar;
+              document.getElementById("login").style.display = "none";
+              document.getElementById("header").style.display = "none";
+              document.getElementById("after_loginName").style.display = "inline";
+              document.getElementById("userName").innerHTML = userName;
+              document.getElementById("after_login").style.display = "inline";
+              // window.location.href = "studen t.html";
+          })
+  };
 
-// uPort request credentials
-const uportConnect = function () {
-    duo_connector.requestCredentials({
-        requested: ['name', 'phone', 'country', 'avatar'],
-        // callbackUrl:'student.html',
-        notifications: true // We want this if we want to recieve credentials
-    })
-        .then((credentials) => {
-            // Do something (in this case print all credentials)
-            console.log("Credentials:", credentials);
+  // attest/issue badge
+  const uportAttest = function () {
+      lab_connector.attestCredentials({
+          sub: user_data.uportId,
+          claim: {Blockchainquiz: {naam: 'LABMASTER', description: 'Je had alle vragen goed bij de blockchainquiz en daarmee heb je de titel "LABMASTER" verdient.' }},
+          // callbackUrl: 'student2.html',
+          exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
+          // uriHandler: (log) => { console.log(log)}
+          })
+          .then((attestation) => {
+          console.log("Attestation = " + attestation);
+          // document.getElementById("scoreboard").style.display = "inline";
+          document.getElementById("after_claim").style.display = "inline";
+          document.getElementById("after_submit").style.display = "none";
+          document.getElementById("userName").style.display = "none";
+          readCounter();
+      })
+  };
 
-            user_data.uportId = credentials.address;
-            user_data.uportName = credentials.name;
-            user_data.uportCountry = credentials.country;
-            user_data.uportPhone = credentials.phone;
-            var userName = user_data.uportName;
-            document.getElementById("login").style.display = "none";
-            document.getElementById("header").style.display = "none";
-            document.getElementById("after_loginName").style.display = "inline";
-            document.getElementById("userName").innerHTML = userName;
-            document.getElementById("after_login").style.display = "inline";
-            // window.location.href = "studen t.html";
-        })
-};
+  // attest/issue badge
+  const uportAttest1 = function () {
+      lab_connector.attestCredentials({
+          sub: user_data.uportId,
+          claim: {Blockchainquiz: {naam: 'LABORANT', description: 'Je bent goed op de hoogte wat betreft blockchain, je verdient de title "LABORANT"' }},
+          // callbackUrl: 'student2.html',
+          exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
+          // uriHandler: (log) => { console.log(log)}
+          })
+          .then((attestation) => {
+          console.log("Attestation = " + attestation);
+          document.getElementById("after_claim").style.display = "inline";
+          document.getElementById("after_submit").style.display = "none";
+          document.getElementById("userName").style.display = "none";
+          readCounter();
+      })
+  };
 
-// attest/issue badge
-const uportAttest = function () {
-    duo_connector.attestCredentials({
-        sub: user_data.uportId,
-        claim: {Test: {naam: 'Uitslag Quiz', description: 'Uitslag Quiz' }},
-        // callbackUrl: 'student2.html',
-        exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
-        // uriHandler: (log) => { console.log(log)}
-        })
-        .then((attestation) => {
-        console.log("Attestation = " + attestation);
-        window.location.href = "index.html";
-    })
-};
+  // attest/issue badge
+  const uportAttest2 = function () {
+      lab_connector.attestCredentials({
+          sub: user_data.uportId,
+          claim: {Blockchainquiz: {naam: 'LABASSISTENT', description: 'De basis is er, nu nog even doorleren. Je krijgt de titel "LABASSISTENT"' }},
+          // callbackUrl: 'student2.html',
+          exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
+          // uriHandler: (log) => { console.log(log)}
+          })
+          .then((attestation) => {
+          console.log("Attestation = " + attestation);
+          document.getElementById("after_claim").style.display = "inline";
+          document.getElementById("after_submit").style.display = "none";
+          document.getElementById("userName").style.display = "none";
+          readCounter();
+      })
+  };
+
+  // attest/issue badge 3
+  const uportAttest3 = function () {
+      lab_connector.attestCredentials({
+          sub: user_data.uportId,
+          claim: {Blockchainquiz  : {naam: 'LABVEGER', description: 'Er is nog veel ruimte voor verbetering, je mag jezelf "LABVEGER" noemen' }},
+          exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
+          })
+          .then((attestation) => {
+          console.log("Attestation = " + attestation);
+          // window.location.href = "index.html";
+          document.getElementById("after_claim").style.display = "inline";
+          document.getElementById("after_submit").style.display = "none";
+          document.getElementById("userName").style.display = "none";
+          readCounter();
+          //
+
+      })
+  };
